540c71a4f0545ac6b784b8baa16b9b16bbd89072
Nikolay Plekhanov <kkolyankkolyan@gmail.com>
2020-08-03 06:19:45 +0300

    fix mistakes

--- main.cpp
+++ main.cpp
@@ -6,19 +6,19 @@ int main() {
     JavaVM *jvm;       /* denotes a Java VM */
     JNIEnv *env;       /* pointer to native method interface */
     JavaVMInitArgs vm_args; /* JDK/JRE 10 VM initialization arguments */
-    JavaVMOption *options = new JavaVMOption[1];
+    auto *options = new JavaVMOption[4];
     options[0].optionString = (char*) "-Djava.compiler=NONE";           /* disable JIT */
     options[1].optionString = (char*) "-Djava.class.path=c:/dev/j4cpp"; /* user classes */
     options[2].optionString = (char*) "-Djava.library.path=.";  /* set native library path */
     options[3].optionString = (char*) "-verbose:jni";                   /* print JNI-related messages */
     vm_args.version = JNI_VERSION_1_8;
-    vm_args.nOptions = 1;
+    vm_args.nOptions = 4;
     vm_args.options = options;
     vm_args.ignoreUnrecognized = false;
     std::cout << "Starting VM..." << std::endl;
     JNI_CreateJavaVM(&jvm, (void **) &env, &vm_args);
     std::cout << "VM started" << std::endl;
-    delete options;
+    delete[] options;
 /* invoke the Main.test method using the JNI */
     jclass cls = env->FindClass("J4Cpp");
     jmethodID mid = env->GetStaticMethodID(cls, "test", "()V");
